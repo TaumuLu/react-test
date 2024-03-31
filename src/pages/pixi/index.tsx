54644fa0276def6c672236426962f0f3f6391286
TaumuLu <972409545@qq.com>
2024-03-31 22:11:22 +0800

    fix: upgrade pkg

--- src/pages/pixi/index.tsx
+++ src/pages/pixi/index.tsx
@@ -1,13 +1,13 @@
 import { TestPixiReact } from './pixi-react'
-import { TestPixiReactFiber } from './react-pixi-fiber'
 
+// import { TestPixiReactFiber } from './react-pixi-fiber'
 import './index.scss'
 
 export const PixiReact = () => {
   return (
     <div className='pixi-react-root'>
       <TestPixiReact />
-      <TestPixiReactFiber />
+      {/* <TestPixiReactFiber /> */}
     </div>
   )
 }
--- src/pages/pixi/react-pixi-fiber.tsx
+++ src/pages/pixi/react-pixi-fiber.tsx
@@ -1,55 +1,55 @@
-import { useTimeout } from 'ahooks'
-import { Sprite as ISprite, Texture } from 'pixi.js'
-import { useRef, useState } from 'react'
-import { Container, Sprite, Stage } from 'react-pixi-fiber'
+// import { useTimeout } from 'ahooks'
+// import { Sprite as ISprite, Texture } from 'pixi.js'
+// import { useRef, useState } from 'react'
+// import { Container, Sprite, Stage } from 'react-pixi-fiber'
 
-const TestImg = () => {
-  const scale = 0.5
-  const [x, setX] = useState(0)
-  const ref = useRef<ISprite>()
+// const TestImg = () => {
+//   const scale = 0.5
+//   const [x, setX] = useState(0)
+//   const ref = useRef<ISprite>()
 
-  useTimeout(() => {
-    ref.current.scale.set(1)
-  }, 1000)
+//   useTimeout(() => {
+//     ref.current.scale.set(1)
+//   }, 1000)
 
-  useTimeout(() => {
-    setX(100)
-  }, 2000)
+//   useTimeout(() => {
+//     setX(100)
+//   }, 2000)
 
-  return (
-    <Sprite
-      x={x}
-      ref={ref as any}
-      scale={scale}
-      width={200}
-      height={200}
-      texture={Texture.from('test.jpg')}
-    />
-  )
-}
+//   return (
+//     <Sprite
+//       x={x}
+//       ref={ref as any}
+//       scale={scale}
+//       width={200}
+//       height={200}
+//       texture={Texture.from('test.jpg')}
+//     />
+//   )
+// }
 
-export const TestPixiReactFiber = () => {
-  return (
-    <div>
-      <div>react-pixi-fiber</div>
-      <Stage
-        // onMount={setApp}
-        // onUnmount={() => {}}
-        // renderOnComponentChange={false}
-        options={{
-          width: 600,
-          antialias: true,
-          // resolution: 2,
-          backgroundAlpha: 0,
-          powerPreference: 'high-performance',
-          autoStart: true,
-          autoDensity: true,
-        }}
-      >
-        <Container>
-          <TestImg />
-        </Container>
-      </Stage>
-    </div>
-  )
-}
+// export const TestPixiReactFiber = () => {
+//   return (
+//     <div>
+//       <div>react-pixi-fiber</div>
+//       <Stage
+//         // onMount={setApp}
+//         // onUnmount={() => {}}
+//         // renderOnComponentChange={false}
+//         options={{
+//           width: 600,
+//           antialias: true,
+//           // resolution: 2,
+//           backgroundAlpha: 0,
+//           powerPreference: 'high-performance',
+//           autoStart: true,
+//           autoDensity: true,
+//         }}
+//       >
+//         <Container>
+//           <TestImg />
+//         </Container>
+//       </Stage>
+//     </div>
+//   )
+// }
