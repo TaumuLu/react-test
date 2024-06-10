d2ccc9a53f9ab5ec14336dec35f9196a5727ac0b
TaumuLu <972409545@qq.com>
2024-06-10 18:05:47 +0800

    chore: test svg clip

--- src/pages/svg/nineSlice.tsx
+++ src/pages/svg/nineSlice.tsx
@@ -2,10 +2,48 @@ import { observer } from 'mobx-react-lite'
 
 import Bubble from '@/assets/bubble.svg'
 
+import './nineSlice.scss'
+
 export const Component = observer(() => {
   return (
-    <div>
-      <Bubble />
+    <div className='svg-nineSlice'>
+      <div className='svg-nineSlice-icon'>{/* <Bubble /> */}</div>
+      <div className='svg-nineSlice-clip'>
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clipPath='url(#clip-top-left)' />
+        </svg>
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-top)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-top-right)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-left)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-center)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-right)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-bottom-left)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-bottom)' />
+        </svg>
+
+        <svg viewBox='0 0 21 19'>
+          <use xlinkHref='#Web_Red_3' clip-path='url(#clip-bottom-right)' />
+        </svg>
+      </div>
     </div>
   )
 })
